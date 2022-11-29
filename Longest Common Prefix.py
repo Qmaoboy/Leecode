0b95a87a91a6d7de320004dad65e646e3ee3c420
qmaoboy <hpjhuang@gmail.com>
2022-11-29 16:14:23 +0800

    first promote

---
+++ Longest Common Prefix.py
@@ -0,0 +1,15 @@
+
+def longestCommonPrefix(strs: list[str])-> str:
+    bef = [str[0]].split()
+    for idx,item in enumerate(strs):
+        for i,j in enumerate(item):
+            pass
+    print(bef)
+
+        
+    
+            
+    
+        
+strs =["flower","flow","flight"]
+longestCommonPrefix(strs)
