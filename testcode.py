eff471901e7ff4eb9f36499488370b4478975f51
Qmaoboy <hpjhuang@gmail.com>
2023-03-01 14:24:24 +0800

    i

--- testcode.py
+++ testcode.py
@@ -1,12 +1,35 @@
+from tqdm import tqdm
+import time
+
 def testunit(testsample,ans,Programname):
-    tt=0
-    for i,j in zip(testsample,ans):
-        if j==Programname(i):
-            print(f"Test{tt} Pass")
-        else:
-            print(f"Test{tt} FAIL, result= {Programname(i)}, ans={j}")
-        tt+=1
+    start=time.time()
+    if len(testsample)!=len(ans):
+        raise ValueError("測資異常，請確認資料正確。")
         
+    for i in tqdm(range(len(testsample))):
+        if ans[i]==Programname(testsample[i]):
+            time.sleep(0.05)
+            end=time.time()
+            print(f"Test NO.{i} Pass, 執行時間: {end-start}")
+        else:
+            time.sleep(0.05)
+            print(f"Test NO.{i} FAIL, result= {Programname(testsample[i])}, ans={ans[i]}")
+
+
+def testunit_two_para(testsample,para,ans,Programname):
+    start=time.time()
+    if len(testsample)!=len(ans):
+        raise ValueError("測資異常，請確認資料正確。")
+        
+    for i in tqdm(range(len(testsample))):
+        if ans[i]==Programname(testsample[i],para[i]):
+            time.sleep(0.05)
+            end=time.time()
+            print(f"Test NO.{i} Pass, 執行時間: {end-start}")
+        else:
+            time.sleep(0.05)
+            print(f"Test NO.{i} FAIL, result= {Programname(testsample[i])}, ans={ans[i]}")
+
 if __name__=="__main__":
-    print(" try try")
+    print("請用import 方式使用")
     
